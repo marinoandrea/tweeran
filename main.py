ee024477e4967432b16a9acdbf0cef0ce0f14698
Andrea Marino <am.marinoandrea@gmail.com>
2022-12-14 11:27:35 +0100

    Updated `main.py` with visualization (wip)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 
 import dotenv
+import pandas as pd
 import praw
-import tweepy
 
 import tweeran
 
@@ -27,16 +27,18 @@ RELEVANT_SUBREDDITS = [
     "iranian",
     "IranProtest2022",
     "iranprotests",
-    "iranpolitics"
+    "iranpolitics",
+    "politics",
+    "PoliticalDiscussion",
+    "humanrights",
+    "humanrightsdenied"
 ]
+
+WIKIDATA_EVENT_ID = "Q114065797"  # Mahsa Amini protests
 
 
 def main():
     dotenv.load_dotenv()
-
-    twitter_client_stream = tweepy.StreamingClient(
-        os.getenv("TWITTER_BEARER_TOKEN"))
-    twitter_client_normal = tweepy.Client(os.getenv("TWITTER_BEARER_TOKEN"))
 
     reddit_client = praw.Reddit(
         username=os.getenv("REDDIT_USERNAME"),
@@ -46,25 +48,39 @@ def main():
         user_agent="tweener"
     )
 
-    tweeran.RedditExtractionManager(
-        reddit_client,
-        "raw-data_reddit.tsv",
-        RELEVANT_SUBREDDITS
+    result = tweeran.RedditExtractionManager(
+        client=reddit_client,
+        subreddits=RELEVANT_SUBREDDITS,
+        event_wikidata_id=WIKIDATA_EVENT_ID
     ).run()
 
-    tweeran.TwitterSearchExtractionManager(
-        twitter_client_normal,
-        "raw-data_twitter.tsv",
-        RELEVANT_KEYWORDS,
-        RELEVANT_HASHTAGS
-    ).run()
+    result.comments.to_csv("data/reddit_comments.tsv", sep="\t")
+    result.submissions.to_csv("data/reddit_submissions.tsv", sep="\t")
 
-    tweeran.TwitterStreamingExtractionManager(
-        twitter_client_stream,
-        "raw-data_twitter.tsv",
-        RELEVANT_KEYWORDS,
-        RELEVANT_HASHTAGS
-    ).run()
+    submissions = pd.read_csv("data/reddit_submissions.tsv", sep="\t")
+    submissions["sentiment"] = submissions.apply(lambda x: tweeran.get_sentiment_from_text(x["title"]), axis=1)
+
+    tweeran.plot_reddit_data_over_time(submissions, "num_comments", label="# of Comments")
+    tweeran.plot_reddit_data_over_time_subreddits(submissions, "num_comments", label="# of Comments")
+
+    tweeran.plot_reddit_data_over_time(submissions, "sentiment", label="Sentiment Score")
+    tweeran.plot_reddit_data_over_time_subreddits(submissions, "sentiment", label="Sentiment Score")
+
+    tweeran.plot_entities_frequencies(submissions, ["title", "selftext"])
+
+    """
+    TODO: relevant events for analysis
+    [
+        tweeran.TimelineEvent(date=date(2022, 9, 13), name="Arrest Mahsa Amini"),
+        tweeran.TimelineEvent(date=date(2022, 9, 16), name="Death Mahsa Amini"),
+        tweeran.TimelineEvent(date=date(2022, 9, 17), name="Funeral Protests"),
+        tweeran.TimelineEvent(date=date(2022, 9, 22), name="National Protests"),
+        tweeran.TimelineEvent(date=date(2022, 9, 30), name="Shootout Police Station"),
+        tweeran.TimelineEvent(date=date(2022, 10, 3), name="Supreme Leader Talk"),
+        tweeran.TimelineEvent(date=date(2022, 11, 21), name="Soccer Players Silent During Anthem"),
+        tweeran.TimelineEvent(date=date(2022, 12, 4), name="Iran Abolishes Morality Police")
+    ])
+    """
 
 
 if __name__ == '__main__':
